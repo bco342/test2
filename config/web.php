de8c1ae016982631d399d9cbbbf457d4560f6138
Daily <baton07@gmail.com>
2020-05-01 00:29:53 +0500

    done

--- config/web.php
+++ config/web.php
@@ -25,16 +25,7 @@ $config = [
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
-        ],        
-        //'log' => [
-        //   'traceLevel' => YII_DEBUG ? 3 : 0,
-        //    'targets' => [
-        //        [
-        //            'class' => 'yii\log\FileTarget',
-         //           'levels' => ['error', 'warning'],
-        //        ],
-        //    ],
-        //],
+        ],
         'db' => $db,        
         'urlManager' => [
             'enablePrettyUrl' => true,
